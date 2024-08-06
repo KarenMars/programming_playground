7a5f4605948c34eaad90e923f8a1904544d0abb5
KKKaren <H.Ji@student.tudelft.nl>
2024-08-06 23:17:09 +0800

    0806
    Signed-off-by: KKKaren <H.Ji@student.tudelft.nl>

--- C/calendar/calendar.c
+++ C/calendar/calendar.c
@@ -24,6 +24,7 @@ int MonthDay(int year, int i)
     int month_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int leap_year = IsLeap(year);
 
+
     ret = month_day[i-1];
 
     if (leap_year)
@@ -50,6 +51,19 @@ int TotalDay(int year, int month, int day)
     return sum;
 }
 
+int WeekDay(int y, int m, int d)
+{
+    if (1 == m || 2 == m)
+    {
+        m = m + 12;
+        y = y - 1;
+    }
+
+    int ret;
+    ret = (d + 2*m + 3*(m+1) / 5 + y + y / 4 - y / 100 + y / 400 + 1)%7;
+
+    return ret;    
+}
 
 int main()
 {
@@ -74,9 +88,54 @@ int main()
         return -1;
     }
 
+    // leap year
+
+    int leap_year = IsLeap(year);
+    if (leap_year)
+    {
+        printf("%d is leap year.\n", year);
+    }
+    else
+    {
+        printf("%d is not leap year.\n", year);
+    }
+
+    // total day
+
     int total_day = TotalDay(year, month, day);
 
     printf("%d-%d-%d is the %d th day of year %d\n", year, month, day, total_day, year);
+
+    // week day
+
+    int week_day = WeekDay(year, month, day);
+
+    switch (week_day)
+    {
+        case 0:
+            printf("%d-%d-%d is Sunday\n", year, month, day);
+            break;
+        case 1:
+            printf("%d-%d-%d is Monday\n", year, month, day);
+            break;
+        case 2:
+            printf("%d-%d-%d is Tuesday\n", year, month, day);
+            break;
+        case 3:
+            printf("%d-%d-%d is Wednesday\n", year, month, day);
+            break;
+        case 4:
+            printf("%d-%d-%d Thursday\n", year, month, day);
+            break;
+        case 5:
+            printf("%d-%d-%d Friday\n", year, month, day);
+            break;
+        case 6:
+            printf("%d-%d-%d Saturday\n", year, month, day);
+            break;
+    }
+
+
 
     return 0;
 }
